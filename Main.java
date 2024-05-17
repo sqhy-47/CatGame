a978110afef8784653eeb6e55846e7783325284e
Timmy W <triniteesmith1@gmail.com>
2024-05-17 15:58:01 -0400

    renamed abs.java to Main.java to clarify which file is the Main class

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.util.Random;
-public class abs {
+public class Main {
     public static void main(String[] args){
         
         Scanner scnr = new Scanner(System.in);
